c7138cdce65c2d1d0eaf221049daef6589a33568
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-05-16 21:05:57 +0900

    article 終了

--- js/style.js
+++ js/style.js
@@ -1,14 +1,13 @@
-$(function() {
- 
-    // padding内側の横幅を取得し、変数に格納
-    const w = $('.minipicture').width();
-   
-    // コンソールログに表示
-    console.log(w);
- 
-    const change_W = w * 2 / 3
-   
-    console.log(change_W);
+$(function () {
+  // padding内側の横幅を取得し、変数に格納
+  const w = $(".l-contents_main_article_new_img").height();
 
-  jQuery('.minipicture').css('height', change_W);
+  // コンソールログに表示
+  console.log(w);
+
+  const change_W = w + 80;
+
+  console.log(change_W);
+
+  jQuery(".l-contents").css("height", change_W);
 });
